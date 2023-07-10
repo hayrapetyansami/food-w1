24c581dde94e09ecac68634f21959b4ab68f9616
hayrapetyansami <samihayrapetyan@gmail.com>
2023-07-10 14:04:55 +0400

    created timer logic and fixed somme bugs

--- js/index.js
+++ js/index.js
@@ -1,8 +1,8 @@
 window.addEventListener("DOMContentLoaded", function () {
 	// tab logic start
-	const tabsHeader = document.querySelectorAll(".tabheader__item");
+	const tabsHeaders = document.querySelectorAll(".tabheader__item");
 	const tabsContents = document.querySelectorAll(".tabcontent");
-	const tabsHeaderParent = document.querySelector(".tabheader__items");
+	const tabsHeadersParent = document.querySelector(".tabheader__items");
 
 	function hideTabContent() {
 		tabsContents.forEach(tabContent => {
@@ -10,21 +10,21 @@ window.addEventListener("DOMContentLoaded", function () {
 			tabContent.classList.remove("show", "fade");
 		});
 
-		tabsHeader.forEach(tabHeader => tabHeader.classList.remove("tabheader__item_active"));
+		tabsHeaders.forEach(tabHeader => tabHeader.classList.remove("tabheader__item_active"));
 	}
 
 	function showTabContent(i = 0) {
 		tabsContents[i].classList.add("show", "fade");
 		tabsContents[i].classList.remove("hide");
-		tabsHeader[i].classList.add("tabheader__item_active");
+		tabsHeaders[i].classList.add("tabheader__item_active");
 	}
 
 	hideTabContent();
 	showTabContent();
 
-	tabsHeaderParent.addEventListener("click", (e) => {
+	tabsHeadersParent.addEventListener("click", (e) => {
 		if (e.target && e.target.matches(".tabheader__item")) {
-			tabsHeader.forEach((tabHeader, index) => {
+			tabsHeaders.forEach((tabHeader, index) => {
 				if (e.target === tabHeader) {
 					hideTabContent();
 					showTabContent(index);
@@ -34,4 +34,62 @@ window.addEventListener("DOMContentLoaded", function () {
 	});
 
 	// tab logic end
+
+	// timer logic start
+	function getTimeRemaining(endtime) {
+		const total = Date.parse(endtime) - Date.parse(new Date());
+		let days, hours, minutes, seconds;
+
+		if (total <= 0) {
+			days = 0;
+			hours = 0;
+			minutes = 0;
+			seconds = 0;
+		} else {
+			days = Math.floor(total / (1000 * 60 * 60 * 24));
+			hours = Math.floor((total / (1000 * 60 * 60) % 24));
+			minutes = Math.floor((total / 1000 / 60) % 60);
+			seconds = Math.floor((total / 1000) % 60);
+		}
+
+		return {
+			total,
+			days,
+			hours,
+			minutes,
+			seconds
+		}
+	}
+
+	function setZero (n) {
+		return n >= 0 && n < 10 ? `0${n}` : n;
+	}
+
+	function setClock (selector, endtime) {
+		const timer = document.querySelector(selector);
+		const daysBlock = timer.querySelector("#days");
+		const hoursBlock = timer.querySelector("#hours");
+		const minutesBlock = timer.querySelector("#minutes");
+		const secondsBlock = timer.querySelector("#seconds");
+		const timerId = setInterval(updateClock, 1000);
+
+		updateClock();
+
+		function updateClock () {
+			const { total, days, hours, minutes, seconds } = getTimeRemaining(endtime);
+
+			daysBlock.textContent = setZero(days);
+			hoursBlock.textContent = setZero(hours);
+			minutesBlock.textContent = setZero(minutes);
+			secondsBlock.textContent = setZero(seconds);
+
+			if (total <= 0) {
+				clearInterval(timerId);
+			}
+		}
+	}
+
+	setClock(".timer", "2023-07-17");
+
+	// timer logic end
 });
