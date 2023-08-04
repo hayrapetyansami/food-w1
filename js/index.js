1ab34415047340ea7feded5c174dff8bad8f4621
hayrapetyansami <samihayrapetyan@gmail.com>
2023-08-04 13:09:52 +0400

    fixed slider ompitimzation issue

--- js/index.js
+++ js/index.js
@@ -389,15 +389,9 @@ window.addEventListener("DOMContentLoaded", function () {
 	const slidesField = document.querySelector(".offer__slider-inner");
 	const width = window.getComputedStyle(slidesWrapper).width;
 
-	if (slides.length < 10) {
-		total.textContent = `0${slides.length}`;
-		current.textContent = `0${slideIndex}`;
-	} else {
-		total.textContent = slides.length;
-		current.textContent = slideIndex;
-	}
+	checkForZero();
 
-	slidesField.style.cssText = `
+ 	slidesField.style.cssText = `
 		display: flex;
 		width: ${100 * slides.length + "%"};
 		transition: all 0.5s;
@@ -407,15 +401,12 @@ window.addEventListener("DOMContentLoaded", function () {
 		overflow: hidden;
 	`;
 
-	slides.forEach(slide => {
-		slide.style.width = width;
-	});
+	slides.forEach(slide => slide.style.width = width);
 
 	slider.style.position = "relative";
 
 	const indicators = document.createElement("ul");
 	const dots = [];
-	indicators.classList.add("carousel-indicators");
 	indicators.style.cssText = `
 		position: absolute;
 		right: 0;
@@ -457,51 +448,15 @@ window.addEventListener("DOMContentLoaded", function () {
 	}
 
 	nextBtn.addEventListener("click", () => {
-		if (offset === parseFloat(width.slice(0, width.length - 2)) * (slides.length - 1)) {
-			offset = 0;
-		} else {
-			offset += parseFloat(width.slice(0, width.length - 2));
-		}
-		slidesField.style.transform = `translateX(-${offset}px)`;
-
-		if (slideIndex === slides.length || slideIndex >= slides.length) {
-			slideIndex = 1;
-		} else {
-			slideIndex++;
-		}
-
-		if (slides.length < 10) {
-			current.textContent = `0${slideIndex}`;
-		} else {
-			current.textContent = slideIndex;
-		}
-
-		dots.forEach(dot => dot.style.opacity = "0.5");
-		dots[slideIndex - 1].style.opacity = 1;
+		sliderLogic(parseFloat(width.slice(0, width.length - 2)) * (slides.length - 1), true, false);
+		checkForZero();
+		dotsLogic();
 	});
 
 	prevBtn.addEventListener("click", () => {
-		if (offset === 0) {
-			offset = parseFloat(width.slice(0, width.length - 2)) * (slides.length - 1);
-		} else {
-			offset -= parseFloat(width.slice(0, width.length - 2));
-		}
-		slidesField.style.transform = `translateX(-${offset}px)`;
-
-		if (slideIndex === 1 || slideIndex <= 1) {
-			slideIndex = slides.length;
-		} else {
-			slideIndex--;
-		}
-
-		if (slides.length < 10) {
-			current.textContent = `0${slideIndex}`;
-		} else {
-			current.textContent = slideIndex;
-		}
-
-		dots.forEach(dot => dot.style.opacity = "0.5");
-		dots[slideIndex - 1].style.opacity = 1;
+		sliderLogic(0, false, true);
+		checkForZero();
+		dotsLogic();
 	});
 
 	dots.forEach(dot => {
@@ -512,14 +467,38 @@ window.addEventListener("DOMContentLoaded", function () {
 
 			slidesField.style.transform = `translateX(-${offset}px)`;
 
-			if (slides.length < 10) {
-				current.textContent = `0${slideIndex}`;
-			} else {
-				current.textContent = slideIndex;
-			}
-
-			dots.forEach(dot => dot.style.opacity = "0.5");
-			dots[slideIndex - 1].style.opacity = 1;
+			checkForZero();
+			dotsLogic();
 		});
 	});
+
+	function checkForZero() {
+		if (slides.length < 10) {
+			total.textContent = `0${slides.length}`;
+			current.textContent = `0${slideIndex}`;
+		} else {
+			total.textContent = slides.length;
+			current.textContent = slideIndex;
+		}
+	}
+
+	function dotsLogic () {
+		dots.forEach(dot => dot.style.opacity = "0.5");
+		dots[slideIndex - 1].style.opacity = 1;
+	}
+
+	function sliderLogic (statment, next = false, prev = false) {
+		// nextBtn
+		if (next === true && prev === false) {
+			slideIndex === slides.length || slideIndex >= slides.length ? slideIndex = 1 : slideIndex++;
+			offset === statment ? offset = 0 : offset += parseFloat(width.slice(0, width.length - 2));
+		}
+		// prevBtn
+		if (next === false && prev === true) {
+			slideIndex === 1 || slideIndex <= 1 ? slideIndex = slides.length : slideIndex--;
+			offset === statment ? offset = parseFloat(width.slice(0, width.length - 2)) * (slides.length - 1) : offset -= parseFloat(width.slice(0, width.length - 2));
+		}
+
+		slidesField.style.transform = `translateX(-${offset}px)`;
+	}
 });
